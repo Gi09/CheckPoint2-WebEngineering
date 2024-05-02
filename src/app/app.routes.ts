2c4a61d390b6a87a05a210c1294b59fb03309839
Giovanna Sayuri <sayuriantunes@yahoo.com.br>
2024-05-02 11:44:37 -0300

    Criação da API IBGE e Pokemon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ClientDetailComponent } from './components/client-detail/client-detail.
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { TarefaComponent } from './components/tarefa/tarefa.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { IbgeComponent } from './components/ibge/ibge.component';
 
 export const routes: Routes = [
     // Se a path for vazia ele vai para a home
@@ -20,6 +21,7 @@ export const routes: Routes = [
     // Rota para o Pokemon
     { path: 'pokemon', component:PokemonComponent}, 
     // Rota para o IBGE
+    { path: 'ibge', component:IbgeComponent},
     // O ** significa que qualquer outra coisa além do vazio e nova-rota irá para esse
     { path: '**', component: HomeComponent}
 ];
--- src/app/components/ibge/ibge.component.ts
+++ src/app/components/ibge/ibge.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IbgeService } from '../../services/ibge.service';
+
 
 @Component({
   selector: 'app-ibge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ibge.component.html',
   styleUrl: './ibge.component.css'
 })
 export class IbgeComponent {
 
+  estados: any[] = [];
+
+  constructor(private ibgeService: IbgeService){
+    
+  }
+
+  ngOnInit():void{
+    this.get_Estados();
+  }
+
+  get_Estados():void{
+    this.ibgeService.get_Estados().subscribe(estados => this.estados = estados);
+  }
 }
